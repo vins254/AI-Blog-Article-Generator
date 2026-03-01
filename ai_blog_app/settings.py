bbb9494e35c19b92cb73b27070a5d0d57d291679
vins254 <mainavincent003@gmail.com>
2026-03-01 15:02:15 +0300

    initial commit

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -85,7 +85,11 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
